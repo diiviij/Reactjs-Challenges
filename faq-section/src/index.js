434daaf1a2db0e10ea0d8530ce5a80473314df3b
diiviij <divijj01@gmail.com>
2023-05-28 00:03:56 +0530

    27.05.23

---
+++ faq-section/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Apps from "./Components/App"
+
+ReactDOM.render(
+<Apps />, document.getElementById("root"));
--- hide-box/src/Components/App.jsx
+++ hide-box/src/Components/App.jsx
@@ -9,6 +9,8 @@ export default function App() {
     <div className="container">
       <button onClick={() => setShow((show) => !show)}> Show / Hide</button>
       {show ? <h1>Welcome to React Challenges</h1> : null}
+      
+    
     </div>
   );
 }
