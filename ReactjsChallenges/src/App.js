84b28d0ff03a2480f1366e40bdab2a488a64f8c2
Divij Jain <68986989+diiviij@users.noreply.github.com>
2023-10-24 14:15:30 +0530

    24

---
+++ ReactjsChallenges/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import "./components/Navbar.css";
+import Head from "./components/bars/Head";
+
+const App = () => {
+  return (
+    <>
+      <Head />
+
+      <Navbar />
+    </>
+  );
+};
+
+export default App;
---
+++ ReactjsChallenges/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header class="header alter1-header section text-contrast">
+      <div class="container container"></div>
+    </header>
+  );
+};
+
+export default Navbar;
