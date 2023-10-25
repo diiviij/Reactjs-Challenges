ed562cfe5a8ad1169e0b193a899146800530b17b
Divij Jain <68986989+diiviij@users.noreply.github.com>
2023-10-25 22:56:34 +0530

    25thOcto

--- ReactjsChallenges/src/components/bars/Head.js
+++ ReactjsChallenges/src/components/bars/Head.js
@@ -2,31 +2,15 @@ import React from "react";
 
 const Head = () => {
   return (
-    <div class="navbar navbar-expand-md main-nav navigation fixed-top sidebar-left">
+    <nav class="navbar navbar-expand-md main-nav navigation fixed-top sidebar-left">
       <div class="container">
         <button class="navbar-toggler" type="button">
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="mr-lg-5 navbar-brand" href="/themes/laapp-react/">
-          <img
-            src="https://5studios.net//themes/laapp-react/static/media/logo.ad20194d.png"
-            alt="Laapp"
-            class="logo logo-sticky d-block d-md-none"
-          />
-          <img src="" alt="Laapp" class="logo d-none d-md-block" />
-        </a>
+
         <div class="collapse navbar-collapse">
-          <div class="sidebar-brand">
-            <a href="/themes/laapp-react/">
-              <img
-                src="/themes/laapp-react/static/media/logo.ad20194d.png"
-                alt="Laapp Template"
-                class="logo"
-              />
-            </a>
-          </div>
           <ul class="nav navbar-nav ml-auto navbar-nav">
             <li class="nav-item">
               <a href="#!" class="nav-link">
@@ -56,7 +40,7 @@ const Head = () => {
           </ul>
         </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
